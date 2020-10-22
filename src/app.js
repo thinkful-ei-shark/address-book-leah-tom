ba8f956ea5086cee735c78038d35aafb05a4b1b6
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-10-22 16:54:08 -0400

    val tests and mocha tests

--- src/app.js
+++ src/app.js
@@ -1,34 +1,143 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const { v4: uuid } = require('uuid');
 
-const app = express()
+const app = express();
+
 
 const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+    ? 'tiny'
+    : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json())
+
+const contact = [
+    {
+        id: uuid(),
+        "firstName": "String",
+        "lastName": "String",
+        "address1": "String",
+        "address2": "String",
+        "city": "String",
+        "state": "String",
+        "zip": "Number"
+    },
+
+    {
+        id: uuid(),
+        "firstName": "Tom",
+        "lastName": "Wallace",
+        "address1": "Home",
+        "address2": "Boo",
+        "city": "Foo",
+        "state": "String",
+        "zip": "Number"
+    }
+];
+
+app.get('/address', (req, res) => {
+    res.send(contact);
+});
+
+app.post('/address', (req, res) => {
+    const requiredKeys = ["firstName", "lastName", "address1", "city", "state", "zip"]
+    //const { firstName, lastName, address1, address2 = false, city, state, zip } = req.body;
+    const optionalKeys = ["address2"]
+
+    let contact = {}
+    for (let i = 0; i < requiredKeys.length; i++) {
+        const key = requiredKeys[i]
+        if (!req.body[key]) {
+            return res.status(400).send(`${key} required!`)
+        }
+        contact[key] = req.body[key]
+    }
+
+    for (let i = 0; i < optionalKeys.length; i++) {
+        const key = optionalKeys[i]
+        if (req.body[key]) {
+            contact[key] = req.body[key]
+        }
+    }
 
 
-app.get('/', (req, res) =>{
-    res.send('Hello, boilerplate!')
+    if (contact.state.length >= 3) {
+        return res
+            .status(400)
+            .send('State must be exactly two characters!')
+    }
+
+    if (contact.zip.length !== 5) {
+        return res
+            .status(400)
+            .send('Zip must be exaclty 5 digits!')
+    }
+
+
+
+    console.log(contact)
+    // if (!firstName) {
+    //     return res
+    //         .status(400)
+    //         .send('First Name required!')
+    // }
+
+    // if (!lastName) {
+    //     return res
+    //         .status(400)
+    //         .send('Last Name required!')
+    // }
+
+    // if (!address1) {
+    //     return res
+    //         .status(400)
+    //         .send('Address required!')
+    // }
+
+    // if (!city) {
+    //     return res
+    //         .status(400)
+    //         .send('City required!')
+    // }
+
+    // if (!state) {
+    //     return res
+    //         .status(400)
+    //         .send('State is required!')
+    // }
+
+
+
+    // if (!zip) {
+    //     return res
+    //         .status(400)
+    //         .send('Zip is required!')
+    // }
+
+
+
+    return res.status(201).send(contact)
+
+
 })
+
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if( process.env.NODE_ENV === 'production') {
-        response = { error : { message : 'server error'} }
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } };
     } else {
-        console.error(error)
-        response= {message: error.message, error}
+        console.error(error);
+        response = { message: error.message, error };
     }
-    res.status(500).json(response)
-})
+    res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,29 @@
 
+const { expect } = require('chai')
+const supertest = require('supertest')
 const app = require('../src/app')
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+describe('Get address', () => {
+  it('Should an array of all contacts', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, boilerplate!')
+      .get('/address')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        expect(res.body[0]).to.be.an('object');
+        expect(res.body[0]).to.include.all.keys('firstName', 'lastName');
+      })
+  })
+})
+
+describe('POST address', () => {
+  it('Should return 400 if zip characters != 5', () => {
+    return supertest(app)
+      .post('/address')
+      .set('Content-Type', 'application/json')
+      .send({ firstName: 'tom', lastName: 'wall', address1: '45', address2: false, city: 'fg', state: 'fo', zip: '4444' })
+      .expect(400, 'Zip must be exaclty 5 digits!')
   })
 })
